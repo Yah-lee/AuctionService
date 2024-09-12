8907ea4f56ba646301f944d7157ef7804fbc2a7d
Yah-lee <yahleeperfectlove@gmail.com>
2024-09-13 00:25:22 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
 });
-// test
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
