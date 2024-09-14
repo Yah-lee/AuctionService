2792f923d2937e3bae9c789d0501aac6b2218507
Yah-lee <yahleeperfectlove@gmail.com>
2024-09-15 00:05:39 +0700

    update

--- src/database.ts
+++ src/database.ts
@@ -1,26 +1,8 @@
-import { Request, Response, NextFunction } from 'express';
+import { Sequelize } from "sequelize";
 
-class CustomErrorHandler extends Error {
-  constructor(public status: number, public message: string) {
-    super();
-  }
+const sequelize = new Sequelize("Auction", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
 
-  static alreadyExists(message: string) {
-    return new CustomErrorHandler(409, message);
-  }
-
-  static notFound(message: string = 'Not Found') {
-    return new CustomErrorHandler(404, message);
-  }
-
-  static unAuthorized(message: string = 'Unauthorized') {
-    return new CustomErrorHandler(401, message);
-  }
-}
-
-const errorHandler = (err: CustomErrorHandler, req: Request, res: Response, next: NextFunction) => {
-  res.status(err.status || 500).json({ message: err.message || 'Something went wrong' });
-};
-
-// Export both errorHandler and CustomErrorHandler
-export { CustomErrorHandler, errorHandler };
+export { sequelize };
